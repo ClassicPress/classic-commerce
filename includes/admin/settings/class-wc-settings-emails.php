2e6ea34438a10ced7980a9ae53f36639b77b6576
Simply Computing <46998578+simplycomputing@users.noreply.github.com>
2019-10-06 09:06:27 +1100

    Add github link to footer text

--- includes/admin/settings/class-wc-settings-emails.php
+++ includes/admin/settings/class-wc-settings-emails.php
@@ -133,7 +133,7 @@ class WC_Settings_Emails extends WC_Settings_Page {
 					'css'         => 'width:300px; height: 75px;',
 					'placeholder' => __( 'N/A', 'woocommerce' ),
 					'type'        => 'textarea',
-					'default'     => '{site_title}<br/>Powered by Classic Commerce',
+					'default'     => '{site_title}<br/>Powered by <a href="https://github.com/ClassicPress-research/classic-commerce/">Classic Commerce</a>',
 					'autoload'    => false,
 					'desc_tip'    => true,
 				),
