14d4e94c3a4aa09bc5a742a3bb199d4da51609d1
Simply Computing <46998578+simplycomputing@users.noreply.github.com>
2019-10-21 19:25:42 +1100

    Text on Extensions page including GNU license

--- includes/admin/views/html-admin-page-addons.php
+++ includes/admin/views/html-admin-page-addons.php
@@ -14,15 +14,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<h1>Extensions for Classic Commerce</h1>
 
-		<p>This is a message and disclaimer about compatability of WooCoomerce extensions with Classic Commerce.</p>
+		<p><strong>Important Note:</strong> Although Classic Commerce is a fork of WooCommerce version 3.5.3, all JetPack and WooCommerce Services integration has been removed.</p>
 
-		<p>They <strong>probably</strong> should still work (?) but you will need to check carefully.</p>
+		<p>Many extensions or plugins designed for WooCommerce will still work with Classic Commerce provided they do not rely on Jetpack or WooCommerce Services.</p>
 
-		<p>Note that Classic Commerce does not include JetPack integration.</p>
+		<p><strong>The end user is entirely responsible for choosing, installing, testing and monitoring any extensions or plugins that are needed to provide extra functionality to the Classic Commerce core.</strong></p>
 
-		<p>Maybe include a list here of basic extensions that we know are compatible?</p>
+		<p>Before installing and using any extensions or plugins we strongly recommend that you first work in a test environment. If you are working on a live site please ensure that you have a recent backup.</p>
 
-		<p><?php printf( __( 'A catalog of WooCommerce Extensions can be found here: <a href="%s">WooCommerce Extensions Catalog</a>', 'woocommerce' ), 'https://woocommerce.com/product-category/woocommerce-extensions/' ); ?></p>
+		<p>For discussion and help with finding compatible Classic Commerce addons, use the <a href="https://forums.classicpress.net/c/support/classic-commerce/">ClassicPress community forum</a>. For problems with the Classic Commerce core files please raise an issue via <a href="https://github.com/ClassicPress-research/classic-commerce/issues/">Github issues</a>.</p>
+
+		<p>A catalog of WooCommerce Extensions can be found here: <a href="https://woocommerce.com/product-category/woocommerce-extensions/">WooCommerce Extensions Catalog</a></p>
+
+		<hr />
+
+		<p><strong>GNU General Public License</strong></p>
+
+		<p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License, version 2, as published by the Free Software Foundation.</p>
+
+		<p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</p>
+
+		<p>You should have received a copy of the GNU General Public License along with this program. If not, see <a href="https://www.gnu.org/licenses/">www.gnu.org/licenses</a>.</p>
 
 </div>
 
