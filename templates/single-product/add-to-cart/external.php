84e75afef7887779e87e116582931fdd542c508c
Laurence Bahiirwa <laurence@mediauganda.com>
2019-10-25 18:57:38 +0300

    Template overrides wording

--- templates/single-product/add-to-cart/external.php
+++ templates/single-product/add-to-cart/external.php
@@ -4,11 +4,7 @@
  *
  * This template can be overridden by copying it to yourtheme/woocommerce/single-product/add-to-cart/external.php.
  *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
+ * Template Overrides: https://docs.woocommerce.com/document/template-structure/#section-1
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
