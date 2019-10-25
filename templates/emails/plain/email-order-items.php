4b188d524fcbf468a5507918dce217281578f221
Laurence Bahiirwa <laurence@mediauganda.com>
2019-10-26 01:57:08 +0300

    Remove whitespace

--- templates/emails/plain/email-order-items.php
+++ templates/emails/plain/email-order-items.php
@@ -11,7 +11,7 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @author 	WooThemes
+ * @author  WooThemes
  * @package Classic Commerce/Templates/Emails/Plain
  * @version WC-3.2.0
  */
