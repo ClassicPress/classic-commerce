92a0c1da8e08e8643b1d03572149f94ba6b366af
James Nylen <jnylen@gmail.com>
2020-01-11 17:33:53 +0000

    Restore blank lines

--- classic-commerce.php
+++ classic-commerce.php
@@ -11,12 +11,15 @@
  *
  * @package WooCommerce
  */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
 if ( ! function_exists( 'is_plugin_active' ) ) {
 	include_once ABSPATH . 'wp-admin/includes/plugin.php';
 }
+
 /**
  * Returns error when WooCommerce is detected among the files on the server.
  *
@@ -27,24 +30,33 @@ function cc_wc_already_active_notice() {
 	echo esc_html__( 'You must deactivate WooCommerce before activating Classic Commerce.', 'classic-commerce' );
 	echo '</p></div>';
 }
+
 if ( file_exists( WP_PLUGIN_DIR . '/woocommerce/woocommerce.php' ) && file_exists( WP_PLUGIN_DIR . '/woocommerce/includes/class-woocommerce.php' ) && is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
+
 	// Woocommerce Files already exist. Show an admin notice.
 	add_action( 'admin_notices', 'cc_wc_already_active_notice' );
+
 	// Deactivate Classic Commerce.
 	deactivate_plugins( array( 'classic-commerce/classic-commerce.php' ) );
+
 	// Do not proceed further with Classic Commerce loading.
 	return;
+
 } else {
+
 	// Load the Update Client to manage Classic Commerce updates.
 	include_once dirname( __FILE__ ) . '/includes/class-wc-update-client.php';
+
 	// Define WC_PLUGIN_FILE.
 	if ( ! defined( 'WC_PLUGIN_FILE' ) ) {
 		define( 'WC_PLUGIN_FILE', __FILE__ );
 	}
+
 	// Include the main WooCommerce class.
 	if ( ! class_exists( 'WooCommerce' ) ) {
 		include_once dirname( __FILE__ ) . '/includes/class-woocommerce.php';
 	}
+
 	/**
 	 * Main instance of WooCommerce.
 	 *
@@ -56,6 +68,8 @@ if ( file_exists( WP_PLUGIN_DIR . '/woocommerce/woocommerce.php' ) && file_exist
 	function wc() {
 		return WooCommerce::instance();
 	}
+
 	// Global for backwards compatibility.
 	$GLOBALS['woocommerce'] = wc();
+
 }
