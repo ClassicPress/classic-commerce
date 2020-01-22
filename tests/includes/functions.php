4f5377bc6db1f0b369e0ff8f9c7217f783c9a768
James Nylen <jnylen@gmail.com>
2020-01-21 20:26:10 -0600

    Add caveat to cc_normalize_decimal function

--- tests/includes/functions.php
+++ tests/includes/functions.php
@@ -12,6 +12,8 @@
  *
  * Currently: removes trailing '.' and '0' characters, e.g. '1.00' -> '1'.
  *
+ * DOES NOT attempt to handle e.g. '31.50' -> '31.5' at the moment.
+ *
  * @param string $decimal_str The number to normalize.
  * @return string
  */
