2c1d531596a23c512140d7b0bd146fd4b48ee8f5
Tim Hughes <timbocode@users.noreply.github.com>
2020-01-23 11:57:04 +0000

    Update user agent

--- includes/gateways/paypal/includes/class-wc-gateway-paypal-api-handler.php
+++ includes/gateways/paypal/includes/class-wc-gateway-paypal-api-handler.php
@@ -108,7 +108,7 @@ class WC_Gateway_Paypal_API_Handler {
 				'method'      => 'POST',
 				'body'        => self::get_capture_request( $order, $amount ),
 				'timeout'     => 70,
-				'user-agent'  => 'ClassicCommerce/' . WC()->version,
+				'user-agent'  => 'WooCommerce/3.5.3 (compatible; ClassicCommerce/' . WC()->cc_version . ')',
 				'httpversion' => '1.1',
 			)
 		);
@@ -141,7 +141,7 @@ class WC_Gateway_Paypal_API_Handler {
 				'method'      => 'POST',
 				'body'        => self::get_refund_request( $order, $amount, $reason ),
 				'timeout'     => 70,
-				'user-agent'  => 'ClassicCommerce/' . WC()->version,
+				'user-agent'  => 'WooCommerce/3.5.3 (compatible; ClassicCommerce/' . WC()->cc_version . ')',
 				'httpversion' => '1.1',
 			)
 		);
