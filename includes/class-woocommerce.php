2d50d879eed6edb293621eed02f95789b1bd6031
Tim Hughes <timbocode@users.noreply.github.com>
2020-01-24 09:53:12 +0000

    Add function to generate user agent

--- includes/class-woocommerce.php
+++ includes/class-woocommerce.php
@@ -660,6 +660,16 @@ final class WooCommerce {
 			$wpdb->tables[]             = 'woocommerce_termmeta';
 		}
 	}
+	
+	/**
+	 * Generates user agent
+	 *
+	 * @param string    $webhook Optional webhook (eg Hookshot).
+	 * @return string
+	 */
+	public function user_agent_header( $webhook = '' ) {
+		return sprintf( 'WooCommerce/%s %s (compatible; ClassicCommerce/%s)', WC()->version, $webhook, WC()->cc_version );
+	}
 
 	/**
 	 * Get queue instance.
