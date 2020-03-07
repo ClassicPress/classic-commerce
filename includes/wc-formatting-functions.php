e9c8830a6ae9029ffd52100ac4ce4c10cf5beb1c
Tim Hughes <timbocode@users.noreply.github.com>
2020-03-07 20:33:43 +0000

    Prefix @since versions with "WC-"
    in includes/wc-formatting-functions.php
    
    Co-Authored-By: James Nylen <jnylen@gmail.com>

--- includes/wc-formatting-functions.php
+++ includes/wc-formatting-functions.php
@@ -1354,7 +1354,7 @@ function wc_implode_html_attributes( $raw_attributes ) {
 /**
  * Escape JSON for use on HTML or attribute text nodes.
  *
- * @since 3.5.5
+ * @since WC-3.5.5
  * @param string $json JSON to escape.
  * @param bool   $html True if escaping for HTML text node, false for attributes. Determines how quotes are handled.
  * @return string Escaped JSON.
